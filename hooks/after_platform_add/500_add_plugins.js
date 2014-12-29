0dad2cecb5b7a0860a5da26017c7bd5f8b636ef1
Alex Bundy <abundy@opuslogica.com>
2014-12-29 10:44:50 -0700

    Add ionic hook for adding plugins after platform add

---
+++ hooks/after_platform_add/500_add_plugins.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+// Adds the cordova plugins after running a "platform add"
+
+var sys = require('sys');
+var exec = require('child_process').exec;
+
+// List of plugins to  add
+var plugins = [
+  'com.ionic.keyboard@1.0.3',
+  'com.megster.cordova.bluetoothserial@0.3.4',
+  'org.apache.cordova.console@0.2.12',
+  'org.apache.cordova.device@0.2.13',
+  'org.apache.cordova.device-motion@0.2.11'
+];
+
+for (var c = 0; c < plugins.length; c++) {
+  exec('ionic plugin add ' + plugins[c], function(error, stdout, stderr) {
+    if (error !== null) {
+      sys.print(stderr);
+    } else {
+      sys.print(stsdout);
+    }
+  });
+}
